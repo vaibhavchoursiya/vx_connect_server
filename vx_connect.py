3077d073d6f212f1d4669980deeeb0bf03934049
vaibhav choursiya <vaibhav.choursiya.code@gmail.com>
2025-09-18 18:13:38 +0530

    feat(socket) : created udp socket and seperate the network form worker loop
    - used udp socket to inform about server ip to the client.
    - using worker loop, queue and buffer to manage the continous processing of incomming request.
    - and refactor old code and hardcode things.

--- vx_connect.py
+++ vx_connect.py
@@ -1,6 +1,13 @@
 import socket
+import threading
+import time
 import pyautogui
 import json
+import queue
+
+data_queue = queue.Queue()
+
+udp_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 class Cprint:
     RED = "\033[31m"
@@ -32,12 +39,11 @@ class Events :
 
     @classmethod    
     def on_pan_update(cls,data):
-
-        curr_x, curr_y = pyautogui.position()
         
         x = (data["x"] * cls.width_ratio) * 1.5
         y = (data["y"] * cls.height_ratio) *1.5
-
+        
+        # print(f"x : {x} and y : {y}")
         pyautogui.moveRel(x, y)
 
 
@@ -66,9 +72,49 @@ class Events :
         print(f"W : {cls.width_ratio} H : {cls.height_ratio}")
 
 
+def udp_broadcast():
+    global udp_server
+    UDP_PORT = 36578
+
+    # Create a udp server.
+    # Provider configration to udp socket like allow broadcasting to all networks by 1.
+    udp_server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+
+    while True:
+        message = f"SERVER_HERE".encode()
+        udp_server.sendto(message, ("<broadcast>", UDP_PORT))
+        time.sleep(2)
+
+def worker_loop():
+    while True :
+
+        
+        decodedData =  data_queue.get()
+        if not decodedData:
+            continue;
+        
+        match decodedData["event"]:
+            case "dimenstions":
+                Events.set_ratio(decodedData)
+            case "on_tap_down":
+                Events.on_tap_down()
+            case "on_double_tap_down":
+                Events.on_double_tap_down()
+            case "on_right_click":
+                Events.on_right_click()
+            case "on_pan_update":
+                Events.on_pan_update(decodedData)
+            case "on_keyboard":
+                Events.on_keyboard(decodedData)
+            case _:
+                print("No Event Found:", decodedData)
+
+
 
 def start_vx_connect():
     """Start Your Vx Connect Application Server ;)"""
+    global udp_server
 
     HOST = "0.0.0.0"
     PORT = 8087
@@ -88,48 +134,48 @@ def start_vx_connect():
         if ans != "y" and ans != "yes":
             Cprint.red("Socket Connection Closed :)")
             server_socket.close()
-            return;
-
-
-
+            return
+        
+        buffer = ""
         while True:
+
             data = client.recv(1024).decode("utf-8")
-            try:
-                decodedData = json.loads(data)
-                match decodedData["event"]:
-                    case "dimenstions":
-                        Events.set_ratio(decodedData)
-                    case "on_tap_down":
-                        Events.on_tap_down()
-                    case "on_double_tap_down":
-                        Events.on_double_tap_down()
-                    case "on_right_click":
-                        Events.on_right_click()
-                    case "on_pan_update":
-                        Events.on_pan_update(decodedData)
-                    case "on_keyboard":
-                        Events.on_keyboard(decodedData)
-                    case _:
-                        print("No Event Found:", decodedData)
-            except Exception as e:
-                        print(f"Error: {e}")
-                        server_socket.close();
-                        break;
+
+            if not data:
+                continue
+
+            buffer += data
+
+            while "^" in buffer:
+
+                line, buffer = buffer.split("^", 1)
+                print(line)
+
+                try:
+                    decodedData = json.loads(line)
+                    data_queue.put(decodedData)
+
+                except Exception as e:
+                            print(f"Error: {e}")
+                            udp_server.close()
+                            server_socket.close();
+                            break;
 
                     
 
     except Exception as e:
         server_socket.close();
+        udp_server.close()
         Cprint.red(f"Server error: {e}")
 
-    finally:
-        server_socket.close()
-
+    
 
 
 if __name__ == "__main__":
+    # threading.Thread(target=start_vx_connect, daemon=True).start()
+    threading.Thread(target=udp_broadcast, daemon=True).start()
+    threading.Thread(target=worker_loop, daemon=True).start()
     start_vx_connect()
-    pass
         
 
 
